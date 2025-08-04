99a872c1600a41bc50fdf23eb4af82e3fb26ee15
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2025-08-04 14:27:07 +0100

    Create Skeleton StudyBuddy App Project

---
+++ src/main/java/com/springapplication/studybuddyapp/StudyBuddyAppApplication.java
@@ -0,0 +1,13 @@
+package com.springapplication.studybuddyapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyBuddyAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyBuddyAppApplication.class, args);
+    }
+
+}

bffbfb58da2e6b0344c099ed2411c6edad097011
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2025-08-18 15:41:30 +0100

    Implement testPostgresConnection() method

--- src/test/java/com/springapplication/studybuddyapp/StudyBuddyAppApplicationTests.java
+++ src/test/java/com/springapplication/studybuddyapp/StudyBuddyAppApplicationTests.java
@@ -1,13 +1,30 @@
 package com.springapplication.studybuddyapp;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class StudyBuddyAppApplicationTests {
 
+    @Autowired
+    private DataSource dataSource;
+
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void testPostgresConnection() throws Exception {
+        try (Connection conn = dataSource.getConnection()) {
+            assertNotNull(conn, "Connection should not be null");
+            assertFalse(conn.isClosed(), "Connection should be open");
+        }
     }
 
 }

71fcbfab586cf9072d805b4cb95c03bdaf6cb4e2
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2025-08-19 13:37:09 +0100

    Implement RoleRepository class

--- src/main/java/com/springapplication/studybuddyapp/repository/RoleRepository.java
+++ src/main/java/com/springapplication/studybuddyapp/repository/RoleRepository.java
@@ -1,4 +1,14 @@
 package com.springapplication.studybuddyapp.repository;
 
-public interface RoleRepository {
+import com.springapplication.studybuddyapp.model.Role;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Repository for {@link Role}.
+ */
+public interface RoleRepository extends JpaRepository<Role, Integer> {
+    Optional<Role> findByName(String name);
+    boolean existsByName(String name);
 }
+

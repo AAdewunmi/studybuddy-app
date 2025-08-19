6c2786cd733de952fb2d192ff93d34360582117b
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2025-08-19 13:39:27 +0100

    Implement UserRoleRepository class

--- src/main/java/com/springapplication/studybuddyapp/repository/UserRoleRepository.java
+++ src/main/java/com/springapplication/studybuddyapp/repository/UserRoleRepository.java
@@ -1,4 +1,19 @@
 package com.springapplication.studybuddyapp.repository;
 
-public interface UserRoleRepository {
+import com.springapplication.studybuddyapp.model.UserRole;
+import com.springapplication.studybuddyapp.model.UserRoleId;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Repository for {@link UserRole} join entities.
+ */
+public interface UserRoleRepository extends JpaRepository<UserRole, UserRoleId> {
+
+    List<UserRole> findByUser_Id(Long userId);
+
+    List<UserRole> findByRole_Name(String roleName); // e.g., "ADMIN" or "USER"
+
+    boolean existsByUser_IdAndRole_Name(Long userId, String roleName);
 }
+

b586498133025ddb5fbf74611e357f91ae46b157
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2025-08-20 12:42:46 +0100

    Implement UserResponsedto class

--- src/main/java/com/springapplication/studybuddyapp/repository/UserRepository.java
+++ src/main/java/com/springapplication/studybuddyapp/repository/UserRepository.java
@@ -8,7 +8,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
  * Repository for {@link User}.
  */
 public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByEmail(String email);
-    boolean existsByEmail(String email);
+    Optional<User> findByEmailIgnoreCase(String email);
+    boolean existsByEmailIgnoreCase(String email);
 }
 

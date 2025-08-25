6bb156c09ff55c8ebd14a2fd91140e9a583d0b86
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2025-08-25 20:54:04 +0100

    Create FieldMatch class

--- src/main/java/com/springapplication/studybuddyapp/api/validation/FieldMatch.java
+++ src/main/java/com/springapplication/studybuddyapp/api/validation/FieldMatch.java
@@ -1,4 +1,19 @@
 package com.springapplication.studybuddyapp.api.validation;
 
-public class FieldMatch {
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.*;
+
+@Target({ElementType.TYPE, ElementType.ANNOTATION_TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Constraint(validatedBy = FieldMatchValidator.class)
+public @interface FieldMatch {
+    String message() default "Fields must match";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
+    String first();
+    String second();
 }
+
